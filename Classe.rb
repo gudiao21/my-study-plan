48e5a31541b347f3d4e1c7976efab6635d77ab35
gudiao21 <lucianodantas53@gmail.com>
2022-12-16 20:15:46 -0300

    new

--- Classe.rb
+++ Classe.rb
@@ -1,7 +1,7 @@
 class Animal
-attr_reader :nome #Cria o getter e Lê dentro do atributo "nome" declarado dentro do método como "@nome"
-attr_writer :nome #Cria o getter e escreve dentro do atributo "nome" declarado dentro do método como "@nome"
-attr_accessor :especie #Cria o getter e Lê e escreve dentro do atributo "nome" declarado dentro do método como "@nome"
+attr_reader :nome #Cria o getter e Lê dentro do atributo "nome" declarado dentro do método como "@nome".
+attr_writer :nome #Cria o getter e escreve dentro do atributo "nome" declarado dentro do método como "@nome".
+attr_accessor :especie #Cria o getter e Lê e escreve dentro do atributo "nome" declarado dentro do método como "@nome".
 
   def initialize(n,e)
     @nome = n #Aqui se define o atributo "nome" para o Pet. O "@" significa que é uma variável de instância, ou seja, para cada objeto será possível se criar "nome" distintos.
@@ -10,7 +10,7 @@ attr_accessor :especie #Cria o getter e Lê e escreve dentro do atributo "nome" 
 end
 
 pet = Animal.new(n, e)#Aqui se cria o Objeto pet, ou seja, instancía-se a classe.
-puts "Qual o nome de seu animal de estimação?"
+puts "Qual o nome de seu animal de estimação?"X
 pet.nome = gets.chomp()
 puts "Qual a espécie do pet?"
 pet.especie = gets.chomp()
