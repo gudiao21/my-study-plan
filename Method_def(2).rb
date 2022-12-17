cc5663fd1b428a8fe0b6b78cb74d01d26b413d13
gudiao21 <lucianodantas53@gmail.com>
2022-12-17 10:22:04 -0300

    Commit feito para treinar commit

--- Method_def(2).rb
+++ Method_def(2).rb
@@ -3,9 +3,9 @@ def soma(valor1, valor2 = 0)
     valor1 + valor2
 end    
 
-def soma_com_parametros_nomeados(valor1:, valor2:)
+def soma_com_parametros_nomeados(valor1:, valor2:0)
     soma (valor1, valor2)
 end
 
 puts "Vou executar a soma"
-puts soma_com_parametros_nomeados(valor2: 10, valor1: 5)
+puts soma_com_parametros_nomeados(valor1: 5)
