7303d0ba1d9fffc32a20c98857e00237b2e7b057
gudiao21 <lucianodantas53@gmail.com>
2022-12-17 16:39:59 -0300

    Commit para o arquivo metodo_calculo.rb

---
+++ metodo_calculo.rb
@@ -0,0 +1,20 @@
+def soma(x,y) #Os parênteses poderia ser omitido
+    x+y
+end
+
+def subt(x,y) #Os parênteses poderia ser omitido
+    x-y
+end
+
+def div(x,y) #Os parênteses poderia ser omitido
+    x/y
+end
+
+def mult(x,y) #Os parênteses poderia ser omitido
+    x * y
+end
+
+p soma 2,4
+p subt 5,6
+p mult 3,9
+p div 10,5
