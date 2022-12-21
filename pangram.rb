078152b030b3f0496ed24aa49772ff97fc92e659
gudiao21 <lucianodantas53@gmail.com>
2022-12-21 19:06:13 -0300

    Commit do arquivo pangram.rb

--- pangram.rb
+++ pangram.rb
@@ -1,34 +1,20 @@
-geclass Pangram
-    def initialize(string)
-      @input = string.downcase.split('')
-      @map = {}
-      ('a'..'z').each { |c| @map[c] = nil }
-      infinity = 1.0/0.0
-      @best = { :length => infinity, :string => nil }
-    end
-  
-    def shortest
-      @input.each_with_index do |c, index|
-        @map[c] = index if @map.key?(c)
-        if pangram? and length < @best[:length]
-          @best[:length] = length
-          @best[:string] = value
-        end
-      end
-      @best
-    end
-  
-    def pangram?
-      @map.values.all? { |value| !value.nil? }
-    end
-  
-    def length
-      @map.values.max - @map.values.min
-    end
-  
-    def value
-      @input[@map.values.min..@map.values.max].join('')
-    end
+def is_pangram(string) #Cria um Método com parâmetro chamado "string".
+
+  alpha = ("a".."z").to_a #Cria e armazena na variável "alpha" um array de letras de "a" até "z".Portando "alpha" foi transformado em um array através do comando ".to_a".
+  string.downcase.split("").each do |i| #Para cada elemento do parâmetro "string", que será transformado em minúsculos através do método ".downcase", faça a linha abaixo. O "Split("")" acredito que seria uma forma de ingnorar os espaços encontrados no parâmentro "string", analizando assim letra por letra para cada "i" do parâmetro "string", pois os espaços não são relevantes para a análise em questão!
+    if alpha.include?(i) #Se estiver incluso o respectivo elemento i(cada elemento do parâmetro "string" em "The quick brown ...") no array "alpha".
+      then alpha.delete(i) #Então delete o respectivo elemento i do parâmetro "string" em alpha. 
+    end  
   end
-  pangram = Pangram.new("..")
-  print pangram.shortest
+
+  if alpha.empty? #Se o Array "Alpha" estiver vazio escreva a mensagem abaixo, pois quer dizer que todos os 26 caracteres do array "alpha" foram deletados de um a um para cada correspondente "i" do parâmentro ou argumento chamado "string": "The quick borwn fox  ....".
+    puts "Esta string é um pangram" # Escreva isso se o Array estiver vazio.
+  else
+    puts "Esta String não é um pangram" #Escreva isso se o Array não estiver vazio, ou seja, não sendo deletado todos os elementos de "a" até "z" em alpha, que quer dizer que o parâmetro "string" não é um pangram, porque não contém todas as 26 letras do alfabeto.
+  end
+
+  puts "A quantidade de letras que faltam para ser um pangram são/é: #{alpha.length}" #Imprime a quantidade de elementos do Array "alpha".Se todas as letras foram deletadas retornará zero, e se restar x letras aparecerá o número correspondente a ela.
+  puts "Letra(s) que faltam para ser um pangram são/é: #{alpha}" # imprimir os caracteres que não foram deletados e que impediram que a frase "The quick brown ..." não seja um pangram.
+end
+
+is_pangram("The quick brown fox jumps over the lazy dog!") #chama o Método pelo nome "is_pangram" e passa o parâmetro "string" como " The quick ..."
