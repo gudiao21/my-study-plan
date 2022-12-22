bdb048b8f38e07167c0b9ea6643877de5782aa7e
gudiao21 <lucianodantas53@gmail.com>
2022-12-22 17:50:15 -0300

    Submetendo palindromo.rb

--- palindromo.rb
+++ palindromo.rb
@@ -1,25 +1,27 @@
 def palindromo?(palavra) #O ? é convensionado entre a comunidade quando o método retorna um boleano.
-    palavra = palavra.downcase
-    palavra_reversa = ""
+    palavra = palavra.downcase #A string "palavra" é transformada toda em minúscula.
+    palavra_reversa = "" #Torna a palavra_reversa vazia, eu acho!
+    indice = palavra.length #Variável indice recebe o comprimento da palavra.
 
-    indice = palavra.length
-    until indice == 0 #Fazer até chegar a zero
-        letra = palavra [indice - 1]
+    until indice == 0 #Fazer até chegar a zero, ou seja, fazer até ser verdadeira a igualdade.
+        letra = palavra [indice - 1] # Analiza letra por letra
         palavra_reversa << letra # técnica que adiciona uma letra em uma lista.
         indice -= 1 # que é a mesma coisa de indice = indice -1
     end
-    palavra_reversa == palavra #Ovo == ovo
+   
+    palavra_reversa == palavra # "Palavra_reversa" é igual a "palavra"?
+
 end
 
+puts palindromo?("ovo") #Chamada do método, passando "ovo" como parâmetro.
+puts palindromo?("Ovo") #Chamada do método, passando "Ovo" como parâmetro.
+puts palindromo?("foo") #Chamada do método, passando "foo" como parâmeto.
+
+#Agora abaixo é a forma mais resumida para fazer a mesma coisa:
+
+def palindromo?(palavra)
+    palavra.downcase == palavra.downcase.reverse
+end
 puts palindromo?("ovo")
 puts palindromo?("Ovo")
 puts palindromo?("foo")
-
-#Agora abaixo é a forma mais resumida para fazer a mesma coisa!
-
-#def palindromo?(palavra)
-    #palavra.downcase == palavra.downcase.rev
-#end
-#puts palindromo?("ovo")
-#puts palindromo?("Ovo")
-#puts palindromo?("foo")
