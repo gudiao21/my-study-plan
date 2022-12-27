c145c11d6561efa5bb6fa0ec23f586389da788bc
gudiao21 <lucianodantas53@gmail.com>
2022-12-27 18:06:38 -0300

    Committing VardeClasse.rb file

---
+++ VardeClasse.rb
@@ -0,0 +1,18 @@
+#Programa para exemplificar como é uma "variável de classe".
+class User
+  @@user_count = 0
+  def add(name)
+    puts "User #{name} adicionado"
+    @@user_count += 1
+    puts @@user_count
+  end
+end
+
+first_user = User.new #Cria o objeto "first_user" para o classe "User".
+first_user.add('João') # Chama o método "add" e passando o parâmetro 'João' para a mesma.
+
+second_user = User.new #Cria o objeto "second_user" para o classe "User".
+second_user.add('Mario') # Chama o método "add" e passando o parâmetro 'João' para a mesma.
+
+third_user = User.new #Cria o objeto "third_user" para o classe "User".
+third_user.add('Luciano') # Chama o método "add" e passando o parâmetro 'João' para a mesma.
