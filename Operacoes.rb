aaa64c016a2ef22469521e3c2e36d27de905a316
gudiao21 <lucianodantas53@gmail.com>
2022-12-31 18:57:33 -0300

    Committing Operacoes.rb and case.rb

---
+++ Operacoes.rb
@@ -0,0 +1,14 @@
+puts "Insira um número: "
+x = gets.chomp.to_i
+puts "Insira um outro número: "
+y = gets.chomp.to_i
+
+soma = x + y
+subt = x - y
+mult = x * y
+div = x.to_f / y.to_f
+
+puts soma
+puts = subt
+puts mult
+puts div.to_f
---
+++ case.rb
@@ -0,0 +1,12 @@
+mes = "janeiro"
+
+case mes
+when "janeiro"
+  puts "Tem 31 dias"
+when "fevereiro"
+  puts "Tem 28 dias"
+when "março"
+  puts "Tem 30 dias"
+else
+  puts "Não sei quantos dias esse mês tem!"
+end
