1ae69b1305af3a3d533c69c0e3351d6e84978b7f
gudiao21 <lucianodantas53@gmail.com>
2023-01-02 20:04:19 -0300

    Committing map2.rb file

---
+++ map2.rb
@@ -0,0 +1,6 @@
+nomes = ["Jovier", "Artur", "Daniel", "Rosa"]
+nomes_maiusculas = nomes.map{|nome| nome.upcase}#O  que está entre || é o argumento passado.
+#nomes_maiusculas = nomes.map do |nome|
+#nome.upcase
+#end
+puts nomes_maiusculas
