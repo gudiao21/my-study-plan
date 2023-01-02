afe0b2c5fd7f478e851724f0d8a77ebd54737831
gudiao21 <lucianodantas53@gmail.com>
2023-01-02 20:09:18 -0300

    Committing map3.rb file

--- map3.rb
+++ map3.rb
@@ -1,3 +1,4 @@
+#O método "map" sempre vai retornar um array.
 print [1, 2, 3, 4, 5, 6, 7, 8, 9].map {|numero| numero * 2} #collect = map
 hash = {bacon: "protein", apple: "fruit"}
 print hash.map {|k, v| v.size}
