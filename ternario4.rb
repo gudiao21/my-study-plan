9cd6c48180ab25e85c03f442bc3ef8b8006950ab
gudiao21 <lucianodantas53@gmail.com>
2023-01-03 16:13:52 -0300

    Committing ternario4.rb file

---
+++ ternario4.rb
@@ -0,0 +1,5 @@
+nome = "Luc"
+p resposta = nome.length > 5 ? "Nome é longo" : "Nome é curto."
+
+#Atribui a variável greeting o valor "Bom dia" se a hora atual for menor que 12, senão atribui "Boa tarde."
+p greeting = Time.now.hour < 12 ? "Bom dia" : "Boa tarde"
