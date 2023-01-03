5de44266b3c0278bd4cb350a819b6a6e377f4476
gudiao21 <lucianodantas53@gmail.com>
2023-01-03 15:46:25 -0300

    Committing ternario2.rb file

---
+++ ternario2.rb
@@ -0,0 +1,13 @@
+ anoNascimento = 1980
+ millennial = nil
+
+ if(anoNascimento >= 1981 && anoNascimento <= 1996)
+    p millennial = true
+ else
+    p millennial = false
+ end
+
+#transformando o código acima em Operador Ternário
+#condição ? verdadeiro : falso
+
+p millennial = (anoNascimento >= 1981 && anoNascimento <= 1996) ? true : false
