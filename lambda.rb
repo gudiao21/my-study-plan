b0bd87bab7af9f15a535f14ce1226a9bd98d7c3f
gudiao21 <lucianodantas53@gmail.com>
2023-01-07 11:45:42 -0300

    Committing lambda.rb file

--- lambda.rb
+++ lambda.rb
@@ -5,8 +5,15 @@
 
 # A.call('Danilo') #Chamada do bloco acima pela palavra-chave "call", podendo ou não passar o(s) parâmetro(s), sendo que a quantidades de argumentos passadas tem que ser igual a recebida. Entretanto, nesse caso, foi passado 'Danilo' como parâmetro para o |p|.
 
-#Outra forma de se fazer a mesma coisa.
-A = proc.new do |p| #Poderia ser apenas 'proc' também.
-    puts p
+l = lambda {|param| param * 5} #lambda com apenas 1 parâmetro."para * 5" imprime na tela.
+puts l.call(4)
+
+l = lambda do |p1, p2| #lambda com apenas 2 parâmetros.
+    p1 + p2
 end
-A.call('Danilo')
+
+puts l.call(4, 5)
+
+#Outra forma de se fazer a mesma coisa.
+l = proc.new {|param| param * 5} #Poderia ser apenas 'proc' também.
+    puts l.call(4)
