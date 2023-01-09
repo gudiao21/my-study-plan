daa317a3b12a60f2f693b49f0611ed54e33e82a0
gudiao21 <lucianodantas53@gmail.com>
2023-01-09 17:42:50 -0300

    Committing operadormod.rb file

---
+++ operadormod.rb
@@ -0,0 +1,8 @@
+#Calculadora de operação de módulo
+
+print ('Entre com o primeiro número: ')
+a = gets.to_i
+print('Entre com o segundo número: ')
+b = gets.to_i
+result = a % b
+puts("Esse é o resultado de (#{a} % #{b}) = #{result}")
