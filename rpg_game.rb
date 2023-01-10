d40353df06e71d4f952a163086b9c8c16ac345d5
gudiao21 <lucianodantas53@gmail.com>
2023-01-10 14:49:50 -0300

    Committing rpg_game.rb file

--- rpg_game.rb
+++ rpg_game.rb
@@ -1,7 +1,7 @@
 class Monstro
   attr_accessor :energia, :ataque, :vivo
   def initialize
-    self.energia = Ramdom.rand(10) + 6
+    self.energia = Random.rand(10) + 6
     self.vivo = true
   end  
   
@@ -18,12 +18,38 @@ class Monstro
    
   def esta_vivo?
     true if self.energia > 0
-  end  
+  end
 end
 
-monstro1 = Monstro.new #Instanciando monstro1
-monstro2 = Monstro.new #Instanciando monstro2
-puts monstro1.inspect
-puts monstro2.inspect
+class Heroi
+  attr_accessor :energia, :ataque, :vivo, :numero_de_mortos
+  
+  def initialize
+    self.energia = 30
+    self.vivo = true
+    numero_de_mortos = 0
+  end
 
-monstro1.
+  def bater(alvo)
+    if alvo.esta_vivo?
+      self.ataque = Random.rand(5) + 3 #Tirará no mínimo 3 do oponente.
+      puts "Você acertou o monstro, seu dano foi #{self.ataque}"
+      alvo.energia -= self.ataque
+    else
+      puts "O monstro está morto! \n\n"
+      self.numero_de_mortos += 1
+    end
+  end  
+
+    def esta_vivo?
+      true if self.energia > 0
+    end
+end
+
+odim = Heroi.new #Instanciando monstro1
+fishman = Monstro.new #Instanciando monstro2
+puts odim.inspect
+puts fishman.inspect
+
+odim.bater(fishman)
+puts fishman.inspect
