0856ceafe414200b59a01b2fb5d66519ff6aca55
gudiao21 <lucianodantas53@gmail.com>
2023-01-12 19:56:13 -0300

    Committing String.rb file

--- String.rb
+++ String.rb
@@ -4,21 +4,22 @@ c = 1
 a = "linHa 4 #{b}   " # o sustenido(#), já converte para string quando for número. Além de que para esse tipo de concatenação, tenho que usar "aspas duplas" apenas.
 d = '2'
 e = "aula"
-p "linha 7 #{a}"
+f = "   aprendizado   "
+p "linha 8 #{a}"
 p  a + c.to_s # Diferente do "#" o "c" é preciso converter para string manualmente.
-p 'Linha 9: ' + d #Para concatenar 2 variáveis, as 2 tem que ser do mesmo tipo.
-p (a << b) #Outra forma de concatenação 2 vezes, uma por cada "<".
+p 'Linha 10: ' + d #Para concatenar 2 variáveis, as 2 tem que ser do mesmo tipo.
+p "Linha 11: #{a << b}" #Outra forma de concatenação 2 vezes, uma por cada "<".
 p e.gsub("aula", "linha11") #Substitui "aula" por "linha 11" na String toda.
 p a[0, 4] #Printa 4 posições da String, utilizando-me do método "substring".
 p a.scan(/nossa/) #Encontra a palavra "nossa" no array e printa.
-p a.split("") # Printa uma lista separada por espaço.
-p a.upcase #Deixa as letras todas em maiúsculas.
-p a.downcase #Deixa as letras todas em minúsculas.
-p a.capitalize #Maiuscula apenas a primeira letra da frase.
-p a.delete("aula") #Deleta "aula" de toda a string.
-p a.strip #tira os espaços nas estremidades da string.
-p a.lstrip #Tira os espaços apenas da esquerda (left).
-p a.rstrip #Tira os espaços apenas da direita (right).
-p a.include?("nossa")# Que saber se tem a palavra "nossa" em toda a string.
-p a.index("nossa") #Retorna a posição da palavra "nossa".
-p a.reverse #Escreva o que está em "a" ao contrário.
+p "Linha 14: #{b.split("")}" # Printa uma string em lista separada por espaço, um array!
+p "Linha 16: #{b.upcase}" #Deixa as letras todas em maiúsculas.
+p "Linha 17: #{b.downcase}" #Deixa as letras todas em minúsculas.
+p "Linha 18: #{b.capitalize}" #Maiuscula apenas a primeira letra da frase.
+p "Linha 19: #{b.delete("sário")}" #Deleta "s,á,r,i,o" de toda a string.
+p "Linha 20: #{f.strip}" #tira os espaços nas estremidades da string.
+p "Linha 21: #{f.lstrip}" ##{Tira os espaços apenas da esquerda (left).
+p "Linha 22: #{f.rstrip}" #Tira os espaços apenas da direita (right).
+p "Linha 23: #{a.include?("nossa")}"# Que saber se tem a palavra "nossa" em toda a string.
+p "Linha 24: #{a.index("nossa")}" #Retorna a posição da palavra nossa.
+p "Linha 25: #{f.reverse}" #Escreva o que está em f ao contrário.
