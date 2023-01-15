6eef44caafad2c81df56669b1158027b28f3291c
gudiao21 <lucianodantas53@gmail.com>
2023-01-14 21:16:33 -0300

    Committing Fase1-N2-8.2/binary_search.rb file

--- Fase1-N2-8.2/binary_search.rb
+++ Fase1-N2-8.2/binary_search.rb
@@ -1,6 +1,18 @@
-def binary_search
+def binary_search(list, item)
   low = 0
   high = list.length -1
-  
+    
   while low <= high
     mid = (low + high)/2
+    guess = list[mid]
+    if guess = item
+      return "O indice é: #{mid} e o elemento é: #{guess}"
+    elsif guess > item
+      high = mid - 1
+    else
+      low = mid + 1
+    end
+  end
+  my_list = [2, 3, 5, 7, 9]
+  p binary_search(my_list, 7)
+end              
