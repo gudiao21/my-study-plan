459ce0bd5511ac06dca9f021e723b7406a346b56
gudiao21 <lucianodantas53@gmail.com>
2023-01-15 21:19:55 -0300

    Committing binary_search.rb && counting_sort.rb files

--- Fase1-N2-8.2/binary_search.rb
+++ Fase1-N2-8.2/binary_search.rb
@@ -1,18 +1,18 @@
 def binary_search(list, item)
   low = 0
-  high = list.length -1
+  high = list.length - 1 #"list.length" nos dará a quantidade total de elementos na lista, mas que a nível de "índex", se começa para o primeiro elemento o "0" e para compensar tem que se diminuir 1 para chegar o índex relativo à metade da lista. OBS: Entre "1" e "0" é obrigatório um espaço.
     
   while low <= high
     mid = (low + high)/2 #"mid" será arrendondado para baixo automaticamente pelo Ruby se (low + high) não for um número par.
     guess = list[mid]
-    if guess = item
-      return "O indice é: #{mid} e o elemento é: #{guess}."
+    if guess == item
+      return "O elemento é #{item} e seu índice é #{mid}."
     elsif guess > item
       high = mid - 1
-    else
+    else #Lembrando que o "else" se refere ao que sobrou (guess < item).
       low = mid + 1
     end
   end
+end  
   my_list = [2, 3, 5, 7, 9]
   puts binary_search(my_list, 7)
-end              
---
+++ Fase1-N2-8.2/counting_sort.rb
@@ -0,0 +1,48 @@
+class Countsort
+  attr_reader :array_sorted
+  
+  def initialize
+    @array_sorted = []
+  end
+
+  def init(array)
+    min_max = array_max_min(array)
+    count_sort(array, min_max[0], min_max[1])
+  end
+
+  private
+
+  def array_max_min(array)
+    array.minmax
+  end
+  
+  def count_sort(array, min, max)
+    if min > max
+      return 'algo deu errado, poxa!'
+    else
+      n = max - min + 1
+      size = array.length
+      aux = Array.new(size)
+      count = Array.new(n,0)
+
+      (0...size).each do |i|
+        count[array[i] - min] += 1
+      end
+      (1...n).each do |i|
+        count[i] += count[i - 1]
+      end
+      (0...size).each do |i|
+        aux[count[array[i] - min] - 1] = array[i]
+        count[array[i] - min] -= 1
+      end
+      (0...size).each do |i|
+        array[i] = aux[i]
+      end
+    end
+    @array_sorted = array
+  end
+end
+
+c_s = Countsort.new
+c_s.init([1, 4, 10, 2, 3, 32, 0])
+p c_s.array_sorted
