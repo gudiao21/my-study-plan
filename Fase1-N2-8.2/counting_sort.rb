83a1658a4783207b73e77bdcc3700a6f63686f1f
gudiao21 <lucianodantas53@gmail.com>
2023-01-16 09:21:48 -0300

    Committing counting_sort.rb file

--- Fase1-N2-8.2/counting_sort.rb
+++ Fase1-N2-8.2/counting_sort.rb
@@ -1,48 +1,83 @@
-class Countsort
-  attr_reader :array_sorted
-  
-  def initialize
-    @array_sorted = []
-  end
-
-  def init(array)
-    min_max = array_max_min(array)
-    count_sort(array, min_max[0], min_max[1])
-  end
-
-  private
-
-  def array_max_min(array)
-    array.minmax
-  end
-  
-  def count_sort(array, min, max)
+def counting_sort(a=[9, 6, 8, 7, 6], min  = 0, max = 10)
     if min > max
-      return 'algo deu errado, poxa!'
-    else
-      n = max - min + 1
-      size = array.length
-      aux = Array.new(size)
-      count = Array.new(n,0)
-
-      (0...size).each do |i|
-        count[array[i] - min] += 1
-      end
-      (1...n).each do |i|
-        count[i] += count[i - 1]
-      end
-      (0...size).each do |i|
-        aux[count[array[i] - min] - 1] = array[i]
-        count[array[i] - min] -= 1
-      end
-      (0...size).each do |i|
-        array[i] = aux[i]
-      end
+      return "invalid range"
     end
-    @array_sorted = array
+    
+    n = max - min + 1
+    count = Array.new(n,0)
+    len = a.length
+    output = Array.new(len)
+  
+    for i in 0...len
+      count[a[i] - min] += 1
+    end
+    
+    for i in 1...n
+      count[i] += count[i-1]
+    end
+    
+    for i in 0...len
+      output[count[a[i] - min] - 1] = a[i]
+      count[a[i] - min] -= 1
+    end
+    
+    for i in 0...len
+      a[i] = output[i]
+    end
+    
+    return a
+  
   end
-end
+  
+  p counting_sort
 
-c_s = Countsort.new
-c_s.init([1, 4, 10, 2, 3, 32, 0])
-p c_s.array_sorted
+#outra forma de se fazer o "Counting_sort", entretanto não o entendi!  
+
+# class Countsort
+#   attr_reader :array_sorted
+  
+#   def initialize
+#     @array_sorted = []
+#   end
+
+#   def init(array)
+#     min_max = array_max_min(array)
+#     count_sort(array, min_max[0], min_max[1])
+#   end
+
+#   private
+
+#   def array_max_min(array)
+#     array.minmax
+#   end
+  
+#   def count_sort(array, min, max)
+#     if min > max
+#       return 'algo deu errado, poxa!'
+#     else
+#       n = max - min + 1
+#       size = array.length
+#       aux = Array.new(size)
+#       count = Array.new(n,0)
+
+#       (0...size).each do |i|
+#         count[array[i] - min] += 1
+#       end
+#       (1...n).each do |i|
+#         count[i] += count[i - 1]
+#       end
+#       (0...size).each do |i|
+#         aux[count[array[i] - min] - 1] = array[i]
+#         count[array[i] - min] -= 1
+#       end
+#       (0...size).each do |i|
+#         array[i] = aux[i]
+#       end
+#     end
+#     @array_sorted = array
+#   end
+# end
+
+# c_s = Countsort.new
+# c_s.init([1, 4, 10, 2, 3, 32, 0])
+# p c_s.array_sorted
