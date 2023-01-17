905a88d55515ebfc33aed30fc54d4804292e8456
gudiao21 <lucianodantas53@gmail.com>
2023-01-17 13:40:19 -0300

    Committing counting_sort.rb file

--- Fase1-N2-8.2/counting_sort.rb
+++ Fase1-N2-8.2/counting_sort.rb
@@ -6,11 +6,11 @@ def counting_sort(a=[9, 8, 7, 6, 6], min  = 0, max = 9)
     n = max - min + 1 #n = 9 - 0 + 1 = 10.
     count = Array.new(n,0) #count=[0,0,0,0,0,0,0,0,0,0]
     len = a.length #len = 4
-    output = Array.new(len) #Cria um array com comprimento definido por "len" = 4.
+    output = Array.new(len) #Cria um array com comprimento definido por "len" = 5.
   
     for i in 0...len
       count[a[i] - min] += 1
-    end #Resultaria no final do looping "Count=[0,0,0,0,0,1,1,1,1,1]"
+    end #Resultaria no final do looping "Count=[0,0,0,0,0,0,2,1,1,1]"
     
     for i in 1...n
       count[i] += count[i-1]
