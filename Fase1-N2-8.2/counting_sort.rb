518459848e5860aac839838ca2a7f00035c136d5
gudiao21 <lucianodantas53@gmail.com>
2023-01-18 18:51:24 -0300

    Committing counting_sort.rb file

--- Fase1-N2-8.2/counting_sort.rb
+++ Fase1-N2-8.2/counting_sort.rb
@@ -21,7 +21,7 @@ def counting_sort(a=[9, 8, 7, 6, 6], min  = 0, max = 9)
       count[a[i] - min] -= 1 #"count=[0,0,0,0,0,1,2,3,4]."
     end
     
-    for i in 0...len # len = 5
+    for i in 0...len # len = 5.
       a[i] = output[i]
     end
     
