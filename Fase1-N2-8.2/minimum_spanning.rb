5abee8ad3863a874e56e82eefa9aa17e06980a9f
gudiao21 <lucianodantas53@gmail.com>
2023-01-19 21:10:11 -0300

    Committing miimum_spanning.rb file

--- Fase1-N2-8.2/minimum_spanning.rb
+++ Fase1-N2-8.2/minimum_spanning.rb
@@ -1,36 +1,69 @@
-def minimum_spanning_tree(graph)
-    edges = []
-    graph.each do |vertex, vertex_edges|
-      vertex_edges.each do |edge|
-        edges << [edge[:weight], vertex, edge[:vertex]]
-      end
+# the graph
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
+
+# the costs table
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = Float::INFINITY #
+
+# the parents table
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = nil
+
+@processed = []
+
+def find_lowest_cost_node(costs)
+  lowest_cost = Float::INFINITY
+  lowest_cost_node = nil
+  # Go through each node.
+  costs.each do |node, cost|
+    # If it's the lowest cost so far and hasn't been processed yet...
+    if cost < lowest_cost && !@processed.member?(node)
+      # ... set it as the new lowest-cost node.
+      lowest_cost = cost
+      lowest_cost_node = node
     end
-  
-    edges = edges.sort
-    tree = []
-    parents = (0..5).to_a
-    find_set = lambda { |x| (x == parents[x]) ? x : (parents[x] = find_set.call(parents[x])) }
-    union_set = lambda { |x, y| parents[find_set.call(x)] = find_set.call(y) }
-  
-    edges.each do |edge|
-      weight, a, b = edge
-      a, b = find_set.call(a), find_set.call(b)
-      if a != b
-        tree << edge
-        union_set.call(a, b)
-      end
-    end
-    return tree
   end
-  
-  # Example graph with 6 nodes
-  graph = {
-    0 => [{vertex: 1, weight: 7}, {vertex: 2, weight: 8}, {vertex: 3, weight: 9}],
-    1 => [{vertex: 0, weight: 7}, {vertex: 3, weight: 10}, {vertex: 4, weight: 15}],
-    2 => [{vertex: 0, weight: 8}, {vertex: 3, weight: 11}, {vertex: 5, weight: 7}],
-    3 => [{vertex: 0, weight: 9}, {vertex: 1, weight: 10}, {vertex: 2, weight: 11}, {vertex: 4, weight: 6}, {vertex: 5, weight: 8}],
-    4 => [{vertex: 1, weight: 15}, {vertex: 3, weight: 6}, {vertex: 5, weight: 9}],
-    5 => [{vertex: 2, weight: 7}, {vertex: 3, weight: 8}, {vertex: 4, weight: 9}]
-  }
-  
-  puts "Minimum Spanning Tree: #{minimum_spanning_tree(graph)}"
+  lowest_cost_node
+end
+
+# Find the lowest-cost node that you haven't processed yet.
+node = find_lowest_cost_node(costs)
+# If you've processed all the nodes, this while loop is done.
+until node.nil?
+  cost = costs[node]
+  # Go through all the neighbors of this node.
+  neighbors = graph[node]
+  neighbors.keys.each do |n|
+    new_cost = cost + neighbors[n]
+    # If it's cheaper to get to this neighbor by going through this node...
+    if costs[n] > new_cost
+      # ... update the cost for this node.
+      costs[n] = new_cost
+      # This node becomes the new parent for this neighbor.
+      parents[n] = node
+    end
+  end
+  # Mark the node as processed.
+  @processed << node
+  # Find the next node to process, and loop.
+  node = find_lowest_cost_node(costs)
+end
+
+puts "Cost from the start to each node:"
+puts costs
