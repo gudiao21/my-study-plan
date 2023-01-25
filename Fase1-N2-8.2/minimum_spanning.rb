83237ddfa044f168576ac9bfa15a970bb5c431d1
gudiao21 <lucianodantas53@gmail.com>
2023-01-25 20:32:23 -0300

    Committing teste.rb file

--- Fase1-N2-8.2/minimum_spanning.rb
+++ Fase1-N2-8.2/minimum_spanning.rb
@@ -36,7 +36,7 @@ def find_lowest_cost_node(costs)
   lowest_cost_node = nil
   #Go through each node:
   costs.each do |node, cost| #key = node, value = cost.
-    #If it's the lowest cost so far and hasn't been processed yet ...
+    #If it's the lowest cost so far and hasn't been processed yet...
     if cost < lowest_cost && !@processed.member?(node) #"!" inverts the boolean value, returns true if the elementi is not present in the collection "@processed" and false if it is.
       #... set it as the new lowest-cost node:
       lowest_cost = cost #If the condition is true, "lowest_cost = cost".
