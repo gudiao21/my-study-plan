f9033c5275c65773edc2c0262c6c34b4c2cecb21
gudiao21 <lucianodantas53@gmail.com>
2023-01-28 15:44:49 -0300

    Committing files

--- Fase1-N2-8.2/minimum_sum.rb
+++ Fase1-N2-8.2/minimum_sum.rb
@@ -1,12 +1,13 @@
 def min_sum(arr)
-    arr.sort! #Sort the array without a copy of that.
+    arr.sort! #Sort the array without a copy of that in the memory.
     sum = 0
     for i in 0..arr.length-1
       if i < arr.length/2
         sum += arr[i] * arr[arr.length-1-i]
       else
-        break #Get out of the "for".
+        break #Get out of the "for loop".
       end
+      
     end
     return "This is the minimum sum: #{sum}"
   end
