03c35b266c375cad653b5128a09f1ea60068442f
gudiao21 <lucianodantas53@gmail.com>
2023-02-16 17:18:45 -0300

    files

---
+++ array.rb
@@ -0,0 +1,3 @@
+array = [2,5,4,6]
+array.insert(1, array.delete_at(3))
+p array
---
+++ foodie/lib/foodie.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "foodie/version"
+
+module Foodie
+  class Error < StandardError; end
+  # Your code goes here...
+end
